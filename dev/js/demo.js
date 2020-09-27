73fc0f146929e5dd696b7caf27ced03cfc6165d7
c8flores <63384523+c8flores@users.noreply.github.com>
2020-09-27 16:00:54 -0400

    trying

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -1,4 +1,12 @@
 import {gsap} from "gsap";
 
+var redSpeed = 10;
+var blueSpeed = 10;
 
-console. log(gsap);
+// what do you want to animate? / how long should the animation be? / what is it you want to animate?
+gsap.to(".red-box",{duration: redSpeed, x: 300, borderRadius: "50px"});
+gsap.to(".red-box",{duration: redSpeed, x: 800, borderRadius: "200px", delay: redSpeed});
+
+
+gsap.from(".blue-box",{duration: blueSpeed, x: 300, borderRadius: "100px"});
+gsap.to(".blue-box",{duration: blueSpeed, x: 800, borderRadius: "100px", delay: blueSpeed});
